de704551bd7c75df471c5692cc0f60466ad1446b
Prakash <prakash.bandapalli@gmail.com>
2024-05-17 15:57:26 +0530

    resolved import

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -1,4 +1,4 @@
-import Sidebar from "../components/SideBar";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 import Map from "../components/Map";
 import User from "../components/User";
